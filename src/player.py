85ff2973d381de474811f393733c209d192976e0
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-10-30 12:16:19 -0700

    Overwrite dunder repr method in player class

--- src/player.py
+++ src/player.py
@@ -8,4 +8,7 @@ class Player:
         self.current_room = current_room
 
     def __str__(self):
-        return f"Room: {self.name}, Current room: {self.current_room}"
+        return f"Name: {self.name}, Current room: {self.current_room}"
+
+    def __repr__(self):
+        return f"Player name:({repr(self.name)}), current room:({repr(self.current_room)})"
