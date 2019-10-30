fa63ae50b9c92df8737d46b7b18ed45117ce9264
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-10-30 13:43:33 -0700

    Add cardinal direction attributes

--- src/room.py
+++ src/room.py
@@ -6,6 +6,10 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.n_to = ''
+        self.s_to = ''
+        self.e_to = ''
+        self.w_to = ''
 
     def __str__(self):
         return f"Room: {self.name}\nDescription: {self.description}"
