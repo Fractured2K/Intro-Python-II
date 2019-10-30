d244f758fecb7b24366db9c50a829f361174435d
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-10-30 11:39:36 -0700

    Overwrite dunder str for room

--- src/room.py
+++ src/room.py
@@ -6,3 +6,6 @@ class Room:
     def __int__(self, name, description):
         self.name = name
         self.description = description
+
+    def __str__(self):
+        return f"Room: {self.name}, Description: {self.description}"
