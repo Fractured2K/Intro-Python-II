4b2f8c5f7f3f7bd16eb9f304245b8caf3f49b084
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-10-30 12:30:24 -0700

    Add linebreak to dunder str

--- src/room.py
+++ src/room.py
@@ -8,7 +8,7 @@ class Room:
         self.description = description
 
     def __str__(self):
-        return f"Room: {self.name}, Description: {self.description}"
+        return f"Room: {self.name}\nDescription: {self.description}"
 
     def __repr__(self):
         return f"Room name:({repr(self.name)}), description:({repr(self.description)})"
